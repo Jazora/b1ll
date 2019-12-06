7fafff4f18238f0dfa21e1463a06a169d55602b5
Jazora <jason_weller@hotmail.co.uk>
2019-12-06 11:28:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const Discord = require('discord.js')
 const b1ll = new Discord.Client();
 const token = process.env.bot_token;
 
+bool test = true;
+
 b1ll.on('ready', () => {
     console.log('B1LL is Online');
 });
 
 b1ll.on('message', function (message) {
     var VoiceRole = message.guild.roles.find(role => role.name === "B1LL")
-
-    if (message.member.roles.has(VoiceRole.id)) {
-        message.channel.send(message.content, { tts: true });
-        message.delete();
-    }
+	if test
+	{
+		console.log('TEST TRUE');
+		if (message.member.roles.has(VoiceRole.id)) {
+			message.channel.send(message.content, { tts: true });
+			message.delete();
+		}
+	}
+    
 })
 
 b1ll.login(token);
